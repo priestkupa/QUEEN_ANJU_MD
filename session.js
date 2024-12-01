3ebea98ba607b00437e094e54e03b73e37682a43
priestkupa <samsungmaraire@gmail.com>
2024-12-01 21:12:28 +0200

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qZtlyKpJ#8HA99_ZBbB7_loqiklzfzgla93-2VqueEcFppLkOnQ",
 };
